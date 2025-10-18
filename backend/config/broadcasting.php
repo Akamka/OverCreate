1549fcc81b4112c7f79cab5577e9090198d6aebd
unknown <bashinskiy05work@gmail.com>
2025-10-18 17:22:02 +0200

    back fixes chat

--- backend/config/broadcasting.php
+++ backend/config/broadcasting.php
@@ -4,21 +4,17 @@ return [
 
     'default' => env('BROADCAST_CONNECTION', 'null'),
 
-    'connections' => [
-
-        'pusher' => [
-            'driver' => 'pusher',
-            'key'    => env('PUSHER_APP_KEY', 'local'),
-            'secret' => env('PUSHER_APP_SECRET', 'secret'),
-            'app_id' => env('PUSHER_APP_ID', 'overcreate'),
-            'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
-                'host'    => env('PUSHER_HOST', 'soketi'),
-                'port'    => (int) env('PUSHER_PORT', 6001),
-                'scheme'  => env('PUSHER_SCHEME', 'http'),
-                'useTLS'  => filter_var(env('PUSHER_USETLS', false), FILTER_VALIDATE_BOOL),
-            ],
+'connections' => [
+    'pusher' => [
+        'driver'  => 'pusher',
+        'key'     => env('PUSHER_APP_KEY'),
+        'secret'  => env('PUSHER_APP_SECRET'),
+        'app_id'  => env('PUSHER_APP_ID'),
+        'options' => [
+            'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
+            'useTLS'  => true,
         ],
+    ],
 
         'ably' => [
             'driver' => 'ably',
@@ -38,4 +34,10 @@ return [
             'driver' => 'null',
         ],
     ],
+    
+'options' => [
+  'cluster' => env('PUSHER_APP_CLUSTER','mt1'),
+  'useTLS' => true,
+],
+
 ];
